04cf7306e938e89dec1d942008dbd2e3afd854cd
JaTiTV <info.jatitv@gmail.com>
2021-03-22 01:24:35 +0100

    1.1

--- src/main/java/de/jatitv/papitest/system/JoinEvent.java
+++ src/main/java/de/jatitv/papitest/system/JoinEvent.java
@@ -18,16 +18,11 @@ public class JoinEvent implements Listener {
         if (player.hasPermission("commandgui.admin") || player.isOp()) {
             if (Main.update_version != null) {
                 String foundVersion = Main.getPlugin().getDescription().getVersion();
-                String updateFound = (Main.PrefixHC + "§6A new version of §8[§6Command§9GUI§8]§6 was found!");
+                String updateFound = (Main.PrefixHC + "§6A new version of §8[§5PaPi§6Test§8]§6 was found!");
                 String yourVersion = (Main.PrefixHC + "§6Your version §c" + foundVersion);
                 String currentVersion = (Main.PrefixHC + "§6Current version: §a" + Main.update_version);
                 String downloadVersion = (Main.PrefixHC + "§6You can download it here: §e" + Main.Spigot);
-                String discord = (Main.PrefixHC + "§6You can find more information about §8[§6Command§9GUI§8]§6 on Discord: §e" + Main.DiscordLink);
-                String Snapshot = (Main.PrefixHC + "§4Please note!" +
-                        "\n" + Main.PrefixHC + "§cYou are using the §6" + foundVersion + " §cof §6Command§9GUI!" +
-                        "\n" + Main.PrefixHC + "§cThere may be errors and it is possible that not all functions work as they should!" +
-                        "\n" + Main.PrefixHC + "§2If there are any bugs, please report them to me via Discord so I can fix them." +
-                        "\n" + Main.PrefixHC + "§7" + Main.DiscordLink);
+                String discord = (Main.PrefixHC + "§6You can find more information about §8[§5PaPi§6Test§8]§6 on Discord: §e" + Main.DiscordLink);
 
                 if (Config.UpdateCheckOnJoin) {
                     new BukkitRunnable() {
